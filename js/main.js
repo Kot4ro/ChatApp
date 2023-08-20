89f9ac9afc4f8fb7dd0a3821dfb7bfb2d73751a9
Kot4ro <x21002xx@aitech.ac.jp>
2023-08-21 02:46:27 +0900

    add-JS
    テキストの内容をLiに出力できるようになった。
    CSS修正

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+{
+    const message = document.getElementById('message');
+    const btn = document.getElementById('btn');
+    const you = document.getElementById('you');
+    const li = document.createElement('li');
+    const input = document.querySelector('#add-form input');
+
+    btn.addEventListener('click', () => {
+        you.textContent = message.value;
+        input.value = '';
+        input.focus();
+    });
+}
